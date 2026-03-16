cf918c58b38227b6f34397d7b797d8316dfba3cc
Caleb Myers <cmyers5108@gmail.com>
2026-03-16 01:13:00 -0400

    fix(lint): install missing ESLint dependencies and add React lint rules
    Declares eslint, @typescript-eslint/parser, @typescript-eslint/eslint-plugin,
    eslint-config-prettier, eslint-plugin-react, and eslint-plugin-react-hooks
    in root devDependencies. Adds separate tsx override with React and
    react-hooks plugins. Disables react-in-jsx-scope and prop-types rules.
    
    Refs: task-007
    Worker: worker-3

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,12 +5,39 @@ module.exports = {
   parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
   overrides: [
     {
-      files: ['**/*.ts', '**/*.tsx'],
+      files: ['**/*.ts'],
       parser: '@typescript-eslint/parser',
-      extends: ['eslint:recommended', 'prettier'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+      ],
       plugins: ['@typescript-eslint'],
       parserOptions: { project: true },
-      rules: { '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }] },
+      rules: {
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      },
+    },
+    {
+      files: ['**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'prettier',
+      ],
+      plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+      parserOptions: { project: true, ecmaFeatures: { jsx: true } },
+      settings: { react: { version: 'detect' } },
+      rules: {
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        'react/no-unescaped-entities': 'warn',
+        'react-hooks/set-state-in-effect': 'warn',
+      },
     },
   ],
   ignorePatterns: ['node_modules', 'dist', 'cdk.out', '*.cjs'],
